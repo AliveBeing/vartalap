3263056d304f7d185aa206207051e81f1e8fde7d
Mrinal <mrinalkandwal@gmail.com>
2024-03-14 04:12:53 +0530

    created empty component

--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 function Empty() {
-  return <div>Empty</div>;
+  return (
+    <div className="border-conversation-border border-1 w-full bg-panel-header-background flex flex-col items-center h-[100vh] border-b-icon-green justify-center">
+      <Image src="/whatsapp.gif" alt="Whatsapp" height={300} width={300} />
+    </div>
+  );
 }
 
 export default Empty;
