8345db935b3af4dd7625effdfc79f3e053f75959
AliveBeing <chidvilasvarmaappani@gmail.com>
2024-03-15 04:11:04 +0530

    Upto chat Components

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Avatar from "../common/Avatar";
+
+import { IoVideocam } from "react-icons/io5";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { MdCall } from "react-icons/md";
 
 function ChatHeader() {
   return (
     <div className="n-16 px-4 py-3 flex justify-between items-center bg-panel-header-background z-10">
-      <div className="flex items-center justify-center gap-6"></div>
+      <div className="flex items-center justify-center gap-6">
+        <Avatar type="sm" image={"/profile"}/>
+        <div className="flex flex-col">
+          <span className="text-primary-strong">DEMO</span>
+          <span className="text-secondary text-sm">Onlile/Offline</span>
+        </div>
+      </div>
+      <div className="flex gap-6">
+        <MdCall className="text-panel-header-icon cursor-pointer text-xl" />
+        <IoVideocam className="text-panel-header-icon cursor-pointer text-xl" />
+        <BiSearchAlt2 className="text-panel-header-icon cursor-pointer text-xl" />
+        <BsThreeDotsVertical className="text-panel-header-icon cursor-pointer text-xl" />
+        
+        
+      </div>
     </div>
   );
   
